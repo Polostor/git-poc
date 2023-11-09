567c3acc0c30952260fa888c1c4f137b6c4ea771
Petr Tomanek <petr.tomanek@adastragrp.com>
2023-11-09 11:53:43 +0100

    Initial commit

---
+++ git-poc/src/test/java/cz/peta/git/poc/GitPocApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.peta.git.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

91b98f265e601e4e552a2a810b05033a252df6e8
Petr Tomanek <petr.tomanek@adastragrp.com>
2023-11-09 12:00:40 +0100

    maven

--- src/test/java/cz/peta/git/poc/GitPocApplicationTests.java
+++ src/test/java/cz/peta/git/poc/GitPocApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GitPocApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
